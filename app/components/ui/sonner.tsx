e686d51d09be951efaa94cf67b38350aa7eb024a
Nhan Nguyen Huu <nhnhan@specominc.com>
2026-06-03 17:38:00 +0700

    [STYLES] Change style sonner

--- app/components/ui/sonner.tsx
+++ app/components/ui/sonner.tsx
@@ -1,4 +1,4 @@
-import { CircleCheckIcon, InfoIcon, Loader2Icon, OctagonXIcon, TriangleAlertIcon } from 'lucide-react'
+import { CircleCheckBig, InfoIcon, Loader2Icon, OctagonXIcon, TriangleAlertIcon } from 'lucide-react'
 import { useTheme } from 'next-themes'
 import { Toaster as Sonner, type ToasterProps } from 'sonner'
 
@@ -10,11 +10,11 @@ const Toaster = ({ ...props }: ToasterProps) => {
       theme={theme as ToasterProps['theme']}
       className='toaster group'
       icons={{
-        success: <CircleCheckIcon className='size-4' />,
-        info: <InfoIcon className='size-4' />,
-        warning: <TriangleAlertIcon className='size-4' />,
-        error: <OctagonXIcon className='size-4' />,
-        loading: <Loader2Icon className='size-4 animate-spin' />
+        success: <CircleCheckBig className='size-5' />,
+        info: <InfoIcon className='size-5' />,
+        warning: <TriangleAlertIcon className='size-5' />,
+        error: <OctagonXIcon className='size-5' />,
+        loading: <Loader2Icon className='size-5 animate-spin' />
       }}
       style={
         {
